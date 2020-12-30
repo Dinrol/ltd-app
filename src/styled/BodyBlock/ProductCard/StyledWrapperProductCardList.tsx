793efc20d54c1cfa16c77ab66ee926b59e8b798d
dinrol <rakov0@mail.ru>
2020-12-30 09:17:05 +0300

    fix flex

--- src/styled/BodyBlock/ProductCard/StyledWrapperProductCardList.tsx
+++ src/styled/BodyBlock/ProductCard/StyledWrapperProductCardList.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const StyledWrapperProductCardList = styled.div`
    display:flex;
-   justify-content:space-between;
+   justify-content:space-evenly;
    flex-wrap:wrap;
    margin-bottom:15px;
 `;
